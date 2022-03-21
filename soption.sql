05704eff77563e1545552178a470f84a2b996bac
nimi2838 <bbongsky11@naver.com>
2022-03-21 18:23:30 +0900

    22

--- soption.sql
+++ soption.sql
@@ -7,6 +7,7 @@ CREATE TABLE soption (
 	opPrice	INT
 );
 
+DELETE FROM soption;
 
 INSERT INTO soption VALUES ('sk02','yu1','여드름관리 1회',70000);
 INSERT INTO soption VALUES ('sk02','yu2','여드름관리 3회',180000);
@@ -45,108 +46,37 @@ INSERT INTO soption VALUES ('fa03','lip2','입술+입꼬리 필러',190000);
 INSERT INTO soption VALUES ('li01','sa1','턱보톡스 50유닛(보툴렉스)',19000);
 INSERT INTO soption VALUES ('li01','sa2','턱보톡스 50유닛(수입-엘레간,제오민)',89000);
 
+INSERT INTO soption VALUES ('li02','yun1','윤곽주사 1부위 1회',50000);
+INSERT INTO soption VALUES ('li02','yun2','윤곽주사 1부위 3회',110000);
 
 
-
-
-INSERT INTO soption VALUES ('tox02','ju1','미간/바깥눈가/안쪽눈가/콧등 (부위당/국산)',30000);
-INSERT INTO soption VALUES ('tox02','ju2','이마전체',50000);
-INSERT INTO soption VALUES ('tox02','ju3','턱라인 탄력보톡스',100000);
-INSERT INTO soption VALUES ('tox02','ju4','얼굴아래쪽(모공+자갈턱+턱라인/국산)',160000);
-
-
-
-
-
-INSERT INTO soption VALUES ('tox04','bo1','종아리 200유닛(수입)',660000);
-INSERT INTO soption VALUES ('tox04','bo2','승모근/허벅지/팔뚝 200유닛(수입)',337000);
-
-
-
-INSERT INTO soption VALUES ('fil01','yun1','윤곽주사 1부위 1회',50000);
-INSERT INTO soption VALUES ('fil01','yun2','윤곽주사 1부위 3회',110000);
-
-
-INSERT INTO soption VALUES ('fil02','jo1','조각주사 1회',300000);
-INSERT INTO soption VALUES ('fil02','jo2','조각주사 3회',750000);
+INSERT INTO soption VALUES ('li03','jo1','조각주사 1회',300000);
+INSERT INTO soption VALUES ('li03','jo2','조각주사 3회',750000);
 
 
 
 
 
-
-
-
-INSERT INTO soption VALUES ('las01','fl1','써마지FLX 300샷',1000000);
-INSERT INTO soption VALUES ('las01','fl2','써마지FLX+얼굴전체 데모톡신(국산)',2190000);
-INSERT INTO soption VALUES ('las01','fl3','써마지FLX+얼굴전체 데모톡신(국산)+팔자필러 2cc+실리프트 30줄',2590000);
-
-
-
-INSERT INTO soption VALUES ('las02','ul1','울쎄라 300샷',890000);
-INSERT INTO soption VALUES ('las02','ul2','울쎄라 200샷+턱라인 데모톡신(국산)',690000);
-INSERT INTO soption VALUES ('las02','ul3','울쎄라 300샷+턱라인 데모톡신(국산)',990000);
-
-
-
-
-
-
-INSERT INTO soption VALUES ('col02','la1','레이저토닝 1회',60000);
-INSERT INTO soption VALUES ('col02','la2','레이저토닝 10회',500000);
-
-
-
-INSERT INTO soption VALUES ('sk01','mu1','물광주사 1cc(국산)',65000);
-INSERT INTO soption VALUES ('sk01','mu2','꿀광주사 1cc(수입)',350000);
-
-
-INSERT INTO soption VALUES ('sk02','nc1','NCTF 동안치료(필로가)',90000);
-INSERT INTO soption VALUES ('sk02','nc1','NCTF 동안필+재생1회',150000);
-
-
-INSERT INTO soption VALUES ('sk03','si1','시그널72/아기주사 1회',150000);
-INSERT INTO soption VALUES ('sk03','si2','시그널72/아기주사 3회',400000);
-
-
-INSERT INTO soption VALUES ('sk04','al1','알라딘필링',99000);
-
-
-
-
-INSERT INTO soption VALUES ('bo01','gr1','걸그룹주사 2주(1부위)',400000);
-INSERT INTO soption VALUES ('bo01','gr2','걸그룹주사 4주(1부위)',1000000);
-
+INSERT INTO soption VALUES ('bo01','bo1','종아리 200유닛(수입)',99000);
+INSERT INTO soption VALUES ('bo01','bo2','승모근/허벅지/팔뚝 200유닛(수입)',190000);
 
 
 INSERT INTO soption VALUES ('bo02','bsu','바디슈링크 100샷',40000);
 
+INSERT INTO soption VALUES ('bo03','eg1','엣지주사 1부위 1회',290000);
+INSERT INTO soption VALUES ('bo03','eg2','엣지주사 1부위 3회',590000);
+
+INSERT INTO soption VALUES ('bo04','gr1','걸그룹주사 2주(1부위)',400000);
+INSERT INTO soption VALUES ('bo04','gr2','걸그룹주사 4주(1부위)',1000000);
 
 
-INSERT INTO soption VALUES ('wax01','fc1','인중 5회',50000);
-INSERT INTO soption VALUES ('wax01','fc2','3자 이마 3회',80000);
-INSERT INTO soption VALUES ('wax01','fc3','구렛나루,볼,턱끝,목(턱아래) 5회',130000);
-INSERT INTO soption VALUES ('wax01','fc4','뒷목/얼굴(이마제외) 5회',250000);
 
-
-INSERT INTO soption VALUES ('wax02','gbo1','겨드랑이/항문 5회',30000);
-INSERT INTO soption VALUES ('wax02','gbo2','상완,하완,비키니라인,종아리 5회/항문 10회',200000);
-INSERT INTO soption VALUES ('wax02','gbo3','허벅지,가슴팍,등전체,배,엉덩이,브라질리언 5회',300000);
-INSERT INTO soption VALUES ('wax02','gbo4','종아리+허벅지',490000);
-
-
-INSERT INTO soption VALUES ('wax03','bbo1','겨드랑이 5회',60000);
-INSERT INTO soption VALUES ('wax03','bbo2','상완,하완,가슴팍,,등전체,배,엉덩이,종아리,허벅지 5회',300000);
-INSERT INTO soption VALUES ('wax03','bbo3','종아리+허벅지',590000);
-INSERT INTO soption VALUES ('wax03','bbo4','브라질리언 (항문포함)',500000);
 
 
 
 
 
 SELECT * FROM soption;
-
-
 
 
 
