b77ba8156387b0303d7f3120a5a248e1ed31c847
nimi2838 <bbongsky11@naver.com>
2022-04-05 13:50:33 +0900

    the

--- soption.sql
+++ soption.sql
@@ -34,7 +34,7 @@ INSERT INTO soption VALUES ('fa01','fa3','눈밑필러(수입)',199000);
 
 
 INSERT INTO soption VALUES ('fa02','hi1','하이코',99000);
-INSERT INTO soption VALUES ('fa02','hi2','하이코필러(콧대실+코필러 1회 용량제한없이',139000);
+INSERT INTO soption VALUES ('fa02','hi2','하이코필러(콧대실+코필러 1회 용량제한없이)',139000);
 
 INSERT INTO soption VALUES ('fa03','lip1','입술필러(국산)',59000);
 INSERT INTO soption VALUES ('fa03','lip2','입술+입꼬리 필러',149000);
@@ -43,7 +43,7 @@ INSERT INTO soption VALUES ('fa03','lip2','입술+입꼬리 필러',149000);
 
 
 
-INSERT INTO soption VALUES ('li01','sa1','턱보톡스 50유닛(보툴렉스)',19000);
+INSERT INTO soption VALUES ('li01','sa1','턱보톡스 50유닛(메디톡신)',19000);
 INSERT INTO soption VALUES ('li01','sa2','턱보톡스 50유닛(수입-엘레간,제오민)',89000);
 
 INSERT INTO soption VALUES ('li02','yun1','윤곽주사 1부위 1회',50000);
